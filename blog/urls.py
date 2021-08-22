b17ad3850e30676c43859403321d6f8a8608a72f
ihor_fomin <ihor.fomin.mypage@gmail.com>
2021-08-22 16:10:05 +0300

    Highlighting active links in the site header.

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='home'),
-    path('category/<str:slug>', get_category, name='category'),
+    path('category/<str:slug>/', get_category, name='category'),
 ]
--- static/js/active.js
+++ static/js/active.js
@@ -1,4 +1,13 @@
 $(document).ready(function () {
+
+    $('.menu a').each(function(){
+        let location = window.location.protocol + '//' + window.location.host + window.location.pathname;
+        let link = this.href;
+        if(location == link){
+            $(this).parent().addClass('active');
+        }
+    });
+
     var filterFns = {
         numberGreaterThan50: function () {
             var number = $(this).find('.number').text();
