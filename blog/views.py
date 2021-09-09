7e778bb86da99b0747beb556ecfff5995d31655e
ihor_fomin <ihor.fomin.mypage@gmail.com>
2021-09-09 10:31:26 +0300

    Pagination implementation

--- blog/views.py
+++ blog/views.py
@@ -8,7 +8,7 @@ class Home(ListView):
     model = Post
     template_name = 'blog/index.html'
     context_object_name = 'posts'
-    paginate_by = 2
+    paginate_by = 4
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
