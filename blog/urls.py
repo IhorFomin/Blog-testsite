da2c2a9628ccddc644dd2c8d5f58dc43a9f87832
ihor_fomin <ihor.fomin.mypage@gmail.com>
2021-09-09 10:48:28 +0300

    Implementation of displaying articles by category

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from .views import *
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
-    path('category/<str:slug>/', get_category, name='category'),
+    path('category/<str:slug>/', PostByCategory.as_view(), name='category'),
     path('post/<str:slug>/', get_post, name='post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -16,6 +16,21 @@ class Home(ListView):
         return context
 
 
+class PostByCategory(ListView):
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    paginate_by = 4
+    allow_empty = False
+
+    def get_queryset(self):
+        return Post.objects.filter(category__slug=self.kwargs['slug'])
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = Category.objects.get(slug=self.kwargs['slug'])
+        return context
+
+
 def index(request):
     return render(request, 'blog/index.html')
 
