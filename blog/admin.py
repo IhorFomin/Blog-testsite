d1fd06f69216b7dd9ab063522a8d7cd12544f3ee
ihor_fomin <ihor.fomin.mypage@gmail.com>
2021-09-13 21:27:56 +0300

    Implementation of the output of articles by tags.

--- blog/admin.py
+++ blog/admin.py
@@ -22,7 +22,7 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'slug', 'category', 'created_at', 'get_photo')
     list_display_links = ('id', 'title')
     search_fields = ('title',)
-    list_filter = ('category',)
+    list_filter = ('category', 'tags')
     readonly_fields = ('views', 'created_at', 'get_photo')
     fields = ('title', 'slug', 'category', 'tags', 'content', 'photo', 'get_photo', 'views', 'created_at')
 
--- blog/views.py
+++ blog/views.py
@@ -33,7 +33,18 @@ class PostByCategory(ListView):
 
 
 class PostByTag(ListView):
-    pass
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    paginate_by = 4
+    allow_empty = False
+
+    def get_queryset(self):
+        return Post.objects.filter(tags__slug=self.kwargs['slug'])
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Записи по тегу: ' + str(Tag.objects.get(slug=self.kwargs['slug']))
+        return context
 
 
 class GetPost(DetailView):
