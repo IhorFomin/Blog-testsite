bd139370fa8b57dfb157dbe4ca1c2445ce4effe7
ihor_fomin <ihor.fomin.mypage@gmail.com>
2021-09-22 22:13:52 +0300

    Create a tag cloud and display them on the article view page.

--- blog/templatetags/sidebar.py
+++ blog/templatetags/sidebar.py
@@ -8,3 +8,9 @@ register = template.Library()
 def get_popular(cnt=3):
     posts = Post.objects.order_by('-views')[:cnt]
     return {"posts": posts}
+
+
+@register.inclusion_tag('blog/tags_tpl.html')
+def get_tags():
+    tags = Tag.objects.all()
+    return {"tags": tags}
